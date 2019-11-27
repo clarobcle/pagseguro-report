c06145a60b884974084c363eb65cfd4a6c478485
Cláudio Roberto Clemente <clarobcle@gmail.com>
2019-11-27 17:22:20 -0300

    response.body mostrar ok

--- PagSeguroReport/src/br/com/clarobcle/controller/Url.java
+++ PagSeguroReport/src/br/com/clarobcle/controller/Url.java
@@ -1,8 +1,13 @@
 package br.com.clarobcle.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
 
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
@@ -24,14 +29,20 @@ public class Url {
 		String url = URLDecoder.decode(urlBuilder.build().toString(), StandardCharsets.UTF_8.name());
 
 		//mostrar a url carregada
-		System.out.println(url);
+		//System.out.println(url);
 		
 		Request request = new Request.Builder()
 				.url(url)
 				.build();
 
 		Response response = client.newCall(request).execute();
-		return response.body().string();
+		//guardar o response.body em uma variavel, pois o response.body limpa o body
+		String result = response.body().string();
+		//mostrar o body do html
+		System.out.println(result);
+		//System.out.println("XML response.body : "+result);
+		return result;
 
 	}
+	
 }
